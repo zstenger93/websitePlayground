49597e88c52b46ca08543c93c9fde91c78a17e21
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-07-25 13:19:25 +0200

    how many buttons? yes

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,9 +1,12 @@
 const navbarButtonAbout = document.querySelector(".about-me");
 const navbarButtonContact = document.querySelector(".contact-me");
+const navbarButtonProjects = document.querySelector(".projects");
 const conversationBubbleABM = document.createElement("div");
 const bubbleTextABM = document.createElement("p");
 const conversationBubbleCM = document.createElement("div");
 const bubbleTextCM = document.createElement("p");
+const conversationBubbleP = document.createElement("div");
+const bubbleTextP = document.createElement("p");
 let isButtonClicked = false;
 
 navbarButtonAbout.addEventListener("mouseenter", () => {
@@ -78,3 +81,39 @@ bubbleTextCM.textContent = "The meaning of life is not css for sure.";
 conversationBubbleCM.classList.add("conversation-bubbleCM");
 conversationBubbleCM.appendChild(bubbleTextCM);
 document.body.appendChild(conversationBubbleCM);
+
+navbarButtonProjects.addEventListener("mouseenter", () => {
+	if (!isButtonClicked && navbarButtonProjects.style.animation !== "fall 1s forwards") {
+		navbarButtonProjects.style.animation = "fallP 1s forwards";
+	}
+});
+
+navbarButtonProjects.addEventListener("animationend", () => {
+	if (!isButtonClicked) {
+		navbarButtonProjects.style.animation = "fallP 1s forwards paused";
+
+		conversationBubbleP.style.display = "flex";
+		conversationBubbleP.style.justifyContent = "center";
+		conversationBubbleP.style.alignItems = "center";
+		navbarButtonProjects.classList.add("black-text");
+	}
+});
+
+navbarButtonProjects.addEventListener("click", () => {
+	if (!isButtonClicked) {
+		isButtonClicked = true;
+		navbarButtonProjects.style.animation = "none";
+		handleButtonClickP();
+
+		conversationBubbleP.style.display = "none";
+	}
+});
+
+function handleButtonClickP() {
+	window.open("../html/Projects.html", "_self");
+}
+
+bubbleTextP.textContent = "Actual projectcss.";
+conversationBubbleP.classList.add("conversation-bubbleP");
+conversationBubbleP.appendChild(bubbleTextP);
+document.body.appendChild(conversationBubbleP);
