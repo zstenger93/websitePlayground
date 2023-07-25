8cc40490c969822abee8a443f7967431986cbc81
Zsolt Stenger <zstenger@student.42heilbronn.de>
2023-07-25 09:50:39 +0200

    testing random stuff

--- JavaScript/Home.js
+++ JavaScript/Home.js
@@ -1,4 +1,3 @@
-// JavaScript code
 const navbarButtonAbout = document.querySelector(".about-me");
 const navbarButtonContact = document.querySelector(".contact-me");
 const conversationBubbleABM = document.createElement("div");
@@ -9,15 +8,14 @@ let isButtonClicked = false;
 
 navbarButtonAbout.addEventListener("mouseenter", () => {
 	if (!isButtonClicked && navbarButtonAbout.style.animation !== "fall 1s forwards") {
-		navbarButtonAbout.style.animation = "fall 1s forwards"; // Start the fall animation on hover
+		navbarButtonAbout.style.animation = "fall 1s forwards";
 	}
 });
 
 navbarButtonAbout.addEventListener("animationend", () => {
 	if (!isButtonClicked) {
-		navbarButtonAbout.style.animation = "fall 1s forwards paused"; // Pause the animation when it ends
+		navbarButtonAbout.style.animation = "fall 1s forwards paused";
 
-		// Show the conversation bubble with the text when the animation ends
 		conversationBubbleABM.style.display = "flex";
 		conversationBubbleABM.style.justifyContent = "center";
 		conversationBubbleABM.style.alignItems = "center";
@@ -28,19 +26,17 @@ navbarButtonAbout.addEventListener("animationend", () => {
 navbarButtonAbout.addEventListener("click", () => {
 	if (!isButtonClicked) {
 		isButtonClicked = true;
-		navbarButtonAbout.style.animation = "none"; // Stop the animation on click
-		handleButtonClick(); // Call the function to open a new page
+		navbarButtonAbout.style.animation = "none";
+		handleButtonClick();
 
-		// Hide the conversation bubble when the button is clicked
 		conversationBubbleABM.style.display = "none";
 	}
 });
 
 function handleButtonClick() {
-	window.open("../html/AboutMe.html", "_self"); // Replace the URL with the desired page to open
+	window.open("../html/AboutMe.html", "_self");
 }
 
-// Initialize the conversation bubble with text
 bubbleTextABM.textContent = "Ouch! This CSS is harder than I thought.";
 conversationBubbleABM.classList.add("conversation-bubbleABM");
 conversationBubbleABM.appendChild(bubbleTextABM);
@@ -49,15 +45,14 @@ document.body.appendChild(conversationBubbleABM);
 
 navbarButtonContact.addEventListener("mouseenter", () => {
 	if (!isButtonClicked && navbarButtonContact.style.animation !== "fall 1s forwards") {
-		navbarButtonContact.style.animation = "fallCM 1s forwards"; // Start the fall animation on hover
+		navbarButtonContact.style.animation = "fallCM 1s forwards";
 	}
 });
 
 navbarButtonContact.addEventListener("animationend", () => {
 	if (!isButtonClicked) {
-		navbarButtonContact.style.animation = "fallCM 1s forwards paused"; // Pause the animation when it ends
+		navbarButtonContact.style.animation = "fallCM 1s forwards paused";
 
-		// Show the conversation bubble with the text when the animation ends
 		conversationBubbleCM.style.display = "flex";
 		conversationBubbleCM.style.justifyContent = "center";
 		conversationBubbleCM.style.alignItems = "center";
@@ -68,19 +63,17 @@ navbarButtonContact.addEventListener("animationend", () => {
 navbarButtonContact.addEventListener("click", () => {
 	if (!isButtonClicked) {
 		isButtonClicked = true;
-		navbarButtonContact.style.animation = "none"; // Stop the animation on click
-		handleButtonClickCM(); // Call the function to open a new page
+		navbarButtonContact.style.animation = "none";
+		handleButtonClickCM();
 
-		// Hide the conversation bubble when the button is clicked
 		conversationBubbleCM.style.display = "none";
 	}
 });
 
 function handleButtonClickCM() {
-	window.open("../html/ContanctMe.html", "_self"); // Replace the URL with the desired page to open
+	window.open("../html/ContanctMe.html", "_self");
 }
 
-// Initialize the conversation bubble with text
 bubbleTextCM.textContent = "The meaning of life is not css for sure.";
 conversationBubbleCM.classList.add("conversation-bubbleCM");
 conversationBubbleCM.appendChild(bubbleTextCM);
